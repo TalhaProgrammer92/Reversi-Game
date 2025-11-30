096d2bbb219a039cbd4cbfd5df705d7c9e6eab13
Talha Ahmad <talha.code92@gmail.com>
2025-11-30 22:24:49 +0500

    Reset

--- Domain/Entities/base_entity.py
+++
@@ -1,8 +0,0 @@
-from uuid import uuid4, UUID
-import datetime as dt
-
-class BaseEntity:
-    def __init__(self, **kwargs):
-        self.id: UUID = kwargs.get("id", uuid4())
-        self.created_at: dt.datetime = kwargs.get("created_at", dt.datetime.now())
-        self.updated_at: dt.datetime | None = kwargs.get("updated_at", None)
